e4a0c4832254b2da51dda6e704bfb01298a0703e
Nemanja Gajic <nemanjagajic@Nemanjas-MacBook-Pro.local>
2018-12-13 02:32:23 +0100

    minor styling changes

--- src/screens/finder/AddPet.js
+++ src/screens/finder/AddPet.js
@@ -119,6 +119,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 180,
         marginTop: 20,
+        marginBottom: 5,
         borderWidth: 1,
         borderRadius: 10,
         borderColor: '#ccc',
